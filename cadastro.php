61ea5902975596f54525cf41ea767960dbbf298a
UnderWolrd <queiroznicolas12@gmail.com>
2023-05-16 20:31:00 -0300

    Create cadastro.php

---
+++ cadastro.php
@@ -0,0 +1,33 @@
+<form method="post" name="cliente" action="">
+<label>Nome</label>	
+<input type="text" name="nome" maxlength="150">
+<label>CNPJ</label>	
+<input type="text" name="cnpj" maxlength="15">
+<label>Inscrição Estadual</label>	
+<input type="text" name="inscestadual">
+<label>Responsável</label>	
+<input type="text" name="responsavel">
+<label>CPF</label>	
+<input type="text" name="cpf" maxlength="11">
+<label>RG</label>	
+<input type="text" name="rg" maxlength="15">
+<label>Endereço</label>	
+<input type="text" name="endereco" maxlength="100">
+<label>Numero</label>	
+<input type="text" name="num" maxlength="10">
+<label>Complemento</label>	
+<input type="text" name="numcomp" maxlength="10">
+<label>Bairro</label>	
+<input type="text" name="bairro" maxlength="30">
+<label>Cidade</label>	
+<input type="text" name="cidade" maxlength="30">
+<label>Estado</label>	
+<input type="text" name="estado" maxlength="30">
+<label>Celular</label>	
+<input type="text" name="celular" maxlength="15">
+<label>E-mail</label>	
+<input type="text" name="email" maxlength="50">
+<label>Observações</label>	
+<input type="text" name="obs" maxlength="50">
+<input type="submit" name="enviar" value="Enviar">
+</form>
